c54f44c266d3404d58da0220729e5cf8aed3b031
Soroush Mehraein <soroush@uber.com>
2016-08-03 23:50:08 -0700

    Add relative dates

--- app/src/main/java/com/codepath/apps/twittercproject/TweetsArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/twittercproject/TweetsArrayAdapter.java
@@ -1,11 +1,20 @@
 package com.codepath.apps.twittercproject;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.twittercproject.models.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Author: soroushmehraein
@@ -15,6 +24,39 @@ import java.util.List;
 public class TweetsArrayAdapter extends ArrayAdapter<Tweet> {
 
     public TweetsArrayAdapter(Context context, List<Tweet> objects) {
-        super(context, android.R.layout.simple_list_item_1, objects);
+        super(context, R.layout.item_tweet, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Tweet tweet = getItem(position);
+
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_tweet, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.tvUserName.setText(tweet.getUser().getScreenName());
+        viewHolder.tvBody.setText(tweet.getBody());
+        viewHolder.tvDate.setText(tweet.getCreatedAtRelative());
+        viewHolder.ivProfileImage.setImageResource(android.R.color.transparent);
+        Picasso.with(getContext()).load(tweet.getUser().getProfileImageUrl()).into(viewHolder.ivProfileImage);
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.tvUserName) TextView tvUserName;
+        @BindView(R.id.tvBody) TextView tvBody;
+        @BindView(R.id.ivProfileImage) ImageView ivProfileImage;
+        @BindView(R.id.tvDate) TextView tvDate;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
--- app/src/main/java/com/codepath/apps/twittercproject/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/twittercproject/models/Tweet.java
@@ -1,10 +1,15 @@
 package com.codepath.apps.twittercproject.models;
 
+import android.text.format.DateUtils;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Author: soroushmehraein
@@ -27,6 +32,27 @@ public class Tweet {
 
     public String getCreatedAt() {
         return createdAt;
+    }
+
+    public String getCreatedAtRelative() {
+        return getRelativeTimeAgo(createdAt);
+    }
+
+    private String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 
     public User getUser() {
